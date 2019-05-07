1641e43d28d8f86b0a6cab645cf5169ceecd5840
Sekban <semihersahin@gmail.com>
2019-05-07 21:01:10 +0300

    final trump_cleaned_tweets

--- trumpInitialiser.py
+++ trumpInitialiser.py
@@ -1,11 +1,12 @@
 import re 
 import csv
 import tweepy
+from datetime import datetime
 from textblob import TextBlob
 
 OUTFILE = "trump_cleaned_tweets.csv" 
 output_file = open(OUTFILE, 'w', newline='')
-keys = ['createdAt', 'text', 'favourites', 'sentiment']
+keys = ['timestamp', 'day', 'week', 'tweet', 'sentiment']
 dict_writer = csv.DictWriter(output_file, keys)
 dict_writer.writeheader()
 
@@ -15,6 +16,7 @@ with open('trump_tweets.csv', encoding='utf8') as csvfile:
     '''
     Cleaning retweets
     '''
+    startDate = datetime.strptime('04-28-2013 00:00:00', "%m-%d-%Y %H:%M:%S")
     for row in readCSV:
         'filtering out retweets'
         if row[4] == 'true': 
@@ -23,18 +25,17 @@ with open('trump_tweets.csv', encoding='utf8') as csvfile:
 
         cleanedRow = {}
 
-        cleanedRow['createdAt'] = row[2]
+        cleanedRow['timestamp'] = row[2]
+        tweetDate = datetime.strptime(row[2], "%m-%d-%Y %H:%M:%S")
+        delta = tweetDate - startDate
+        cleanedRow['day'] = delta.days
+        cleanedRow['week'] = int(delta.days / 7)
         'Cleaning Tweet'
-        cleanedRow['text'] = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", row[1]).split())
-        cleanedRow['favourites'] = row[3]
+        cleanedRow['tweet'] = ' '.join(re.sub("([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", row[1]).split())
+        
         'Sentiment Analysis on cleaned tweet'
-        analysis = TextBlob(cleanedRow['text'])
-        if analysis.sentiment.polarity > 0:
-            cleanedRow['sentiment'] = 'positive'
-        elif analysis.sentiment.polarity == 0:
-            cleanedRow['sentiment'] = 'neutral'
-        else:
-            cleanedRow['sentiment'] = 'negative'
+        analysis = TextBlob(cleanedRow['tweet'])
+        cleanedRow['sentiment'] = analysis.sentiment.polarity
 
         cleanedTweets.append(cleanedRow)
     dict_writer.writerows(cleanedTweets)
