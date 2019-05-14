bfab3e60cc844fd0f22f6b9e8774f217224e9385
hasanberatözfidan <berat@hangaarlab.com>
2019-05-14 23:11:47 +0300

    negative tweets decremented by one, column names changed, csv lists created again

--- positive_negative_tweet_seperator_per_day.py
+++ positive_negative_tweet_seperator_per_day.py
@@ -17,7 +17,7 @@ with open('trump_cleaned_tweets.csv', encoding='utf8') as csvfile:
         if sentiment > 0:
             PositiveSentimentPerDay[dayIndex] += 1
         if sentiment < 0:
-            NegativeSentimentPerDay[dayIndex] += 1
+            NegativeSentimentPerDay[dayIndex] -= 1
 
 PositiveSentimentPerDay = PositiveSentimentPerDay.tolist()
 NegativeSentimentPerDay = NegativeSentimentPerDay.tolist()
@@ -27,8 +27,8 @@ outFileForNegativeTweets= "trump_negative_tweets_daily.csv"
 output_file_for_positive_tweets = open(outFileForPositiveTweets, 'w', newline='')
 output_file_for_negative_tweets = open(outFileForNegativeTweets, 'w', newline='')
 
-df = pd.DataFrame(PositiveSentimentPerDay, columns=["aggregated_sentiment_per_day"])
+df = pd.DataFrame(PositiveSentimentPerDay, columns=["positive_tweet_count_per_day"])
 df.to_csv(output_file_for_positive_tweets, index=True)
 
-df = pd.DataFrame(NegativeSentimentPerDay, columns=["aggregated_sentiment_per_day"])
+df = pd.DataFrame(NegativeSentimentPerDay, columns=["negative_tweet_count_per_day"])
 df.to_csv(output_file_for_negative_tweets, index=True)
